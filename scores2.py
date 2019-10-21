63a55b2ebbeb8d5dda97ae42f3694735b0b547f4
Noel Glamann <nglamann@debian08.CIS.COM>
2019-10-21 10:02:07 -0500

    modified:   scores.py 	new file:   scores2.py

---
+++ scores2.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+#10/21/2019
+#Noel & Carter
+
+score_opt = ["1s", "2s", "3s", "4s", "5s", "6s", "3 - Kind", "4 - Kind", "Full House", "Small Straight", "Large Straight", "Chance", "Yahtzee!"]
+
+def ones(lst):
+    this_turn == 0 
+    for items in lst:
+        if item == 1:
+            this_turn == this_turn + 1
+        
+def twos(lst):
+    this_turn == 0 
+    for items in lst:
+        if item == 2:
+            this_turn == this_turn + 2
+            
+def threes(lst):
+    this_turn == 0 
+    for items in lst:
+        if item == 3:
+            this_turn == this_turn + 3
+            
+def fours(lst):
+    this_turn == 0 
+    for items in lst:
+        if item == 4:
+            this_turn == this_turn + 4
+            
+def fives(lst):
+    this_turn == 0 
+    for items in lst:
+        if item == 5:
+            this_turn == this_turn + 5
+            
+def sixs(lst):
+    this_turn == 0 
+    for items in lst:
+        if item == 6:
+            this_turn == this_turn + 6
+            
+            
+    
+    
+    
+    
