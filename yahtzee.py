6f87eefd4916d09800557ae51fc6bf08d24a6997
Carter <ctemm@stcharlessd.org>
2019-10-25 08:48:45 -0500

    Combined everything into one file for simplified use of global variables

--- yahtzee.py
+++ yahtzee.py
@@ -5,13 +5,427 @@
 """An easy-to-play console yahtzee game."""
 
 import random
-import scoring
-from utils import *
 
+
+#Globals used throughout the program
 # List contains scores of each player. first index is always human, the rest refer to number of NPC's.
 scores = []
 # List contains the order of players. first is always human, the rest refer to number of NPC's.
 order = []
+#List contains a list of lists
+#The child lists are categories, in the form of names, already used by each player and/or NPC
+#Each category can only be used once
+used_categories = []
+
+
+#General, multi-purpose functions
+def pluralize(term, should, apostrophe=True):
+	"""Minimalistic function to pluralize a term, if necessary.
+	Only adds "s" to term if should is True."""
+	if should:
+		if apostrophe:
+			return term + "'s"
+		else:
+			return term + "s"
+	else:
+		return term
+
+
+def menu(prompt, items):
+	"""Constructs and shows a simple commandline menu.
+	Returns the user input."""
+	for i in range(len(items)):
+		print(str(i) + ": " + items[i])
+	result = None
+	while True:
+		result = input(prompt)
+		if result:
+			break
+	return result
+
+
+def menu_choice(prompt, items):
+	"""Constructs and shows a simple commandline menu.
+	Returns an int (index of items) that pertains to the user's selection.
+	"""
+	for i in range(len(items)):
+		print(str(i+1) + ": " + items[i])
+	result = None
+	while True:
+		result = input(prompt)
+		if not result.isnumeric():
+			continue
+		result = int(result)
+		if result <= len(items):
+			break
+	return result-1
+
+def roll(dice):
+	"""Rolls dice number of 6-sided dice"""
+	rolls = []
+	for i in range(dice):
+		rolls.append(random.randint(1, 6))
+	return rolls
+
+
+def who(i):
+	"""Returns the name of an NPC if i>0, otherwise you"""
+	if i == 0:
+		return "you"
+	else:
+		return "NPC " + str(i)
+
+
+def turn_order(rolls):
+	"""given a list of roll sums, returns a list of turn order.
+	Turn order is in the form of indexes, where 0=player and everything >0 is NPC's"""
+	order = []
+	while len(order) < len(rolls):
+		maximum = max(rolls)
+		index = rolls.index(maximum)
+		# Remove this maximum from rolls, otherwise every iteration will return the same one
+		# I need caffeination
+		rolls[index] = -1  # We shouldn't run into it again.
+		order.append(index)
+	return order
+
+def is_npc(current_player):
+	return current_player!=0
+
+def remove_all(lst, item):
+	for i in list(lst):
+		if i == item:
+			lst.remove(item)
+	return lst
+
+
+#Functions pertaining to scoring according to yahtzee rules
+
+def best_category(lst):
+	#Certain rolls take priority
+	#Example, chance is really a last ditch option
+	if yahtzee(lst):
+		return yahtzee
+	elif lgst(lst):
+		return lgst
+	elif smst(lst):
+		return smst
+	elif fh(lst):
+		return fh
+	elif fourK(lst):
+		return fourK
+	elif threeK(lst):
+		return threeK
+	#The greatest value yielded after iteration
+	#We're really just trying to see which function gives the highest value, maximizing points
+	maximum = 0
+	maximum_function = None
+	for func in (ones, twos, threes, fours, fives, sixs):
+		result = func(lst)
+		if result>maximum:
+			maximum = result
+			maximum_function = func
+	#Hopefully chance won't even need to be used
+	if chance(lst)>maximum:
+		maximum_function = chance # :(
+	return maximum_function
+
+def usr_scores(lst):
+	this_turn = 0
+	names = []
+	for i, j in categories:
+		names.append(i)
+	category = menu_choice("How would you like to score these dice?", names)
+	category += 1
+
+	if category == 1:
+		return ones(lst)
+
+	elif category == 2:
+		return twos(lst)
+
+	elif category == 3:
+		return threes(lst)
+
+	elif category == 4:
+		return fours(lst)
+	elif category == 5:
+		return fives(lst)
+
+	elif category == 6:
+		return sixs(lst)
+
+	elif category == 7:
+		return threeK(lst)
+
+	elif category == 8:
+		return fourK(lst)
+
+	elif category == 9:
+		return fh(lst)
+
+	elif category == 10:
+		return smst(lst)
+
+	elif category == 11:
+		return lgst(lst)
+
+	elif category == 12:
+		return chance(lst)
+
+	elif category == 13:
+		return yahtzee(lst)
+
+
+# ----------- Processing their score selection! ---------------------
+
+
+def ones(lst):
+	this_turn = 0
+	for item in lst:
+		if item == 1:
+			this_turn = this_turn + 1
+	return this_turn
+
+
+def twos(lst):
+	this_turn = 0
+	for item in lst:
+		if item == 2:
+			this_turn = this_turn + 2
+	return this_turn
+
+
+def threes(lst):
+	this_turn = 0
+	for item in lst:
+		if item == 3:
+			this_turn = this_turn + 3
+	return this_turn
+
+
+def fours(lst):
+	this_turn = 0
+	for item in lst:
+		if item == 4:
+			this_turn = this_turn + 4
+	return this_turn
+
+
+def fives(lst):
+	this_turn = 0
+	for item in lst:
+		if item == 5:
+			this_turn = this_turn + 5
+	return this_turn
+
+
+def sixs(lst):
+	this_turn = 0
+	for item in lst:
+		if item == 6:
+			this_turn = this_turn + 6
+	return this_turn
+
+
+def threeK(lst):
+	if lst.count(1) == 3:
+		return sum(lst)
+	elif lst.count(2) == 3:
+		return sum(lst)
+	elif lst.count(3) == 3:
+		return sum(lst)
+	elif lst.count(4) == 3:
+		return sum(lst)
+	elif lst.count(5) == 3:
+		return sum(lst)
+	elif lst.count(6) == 3:
+		return sum(lst)
+	else:
+		return 0
+
+
+def fourK(lst):
+	if lst.count(1) == 4:
+		return sum(lst)
+	elif lst.count(2) == 4:
+		return sum(lst)
+	elif lst.count(3) == 4:
+		return sum(lst)
+	elif lst.count(4) == 4:
+		return sum(lst)
+	elif lst.count(5) == 4:
+		return sum(lst)
+	elif lst.count(6) == 4:
+		return sum(lst)
+	else:
+		return 0
+
+
+def fh(lst):
+	if lst.count(1) == 3:
+		if lst.count(2) == 2:
+			return 25
+		elif lst.count(3) == 2:
+			return 25
+		elif lst.count(4) == 2:
+			return 25
+		elif lst.count(5) == 2:
+			return 25
+		elif lst.count(6) == 2:
+			return 25
+
+	elif lst.count(2) == 3:
+		if lst.count(1) == 2:
+			return 25
+		elif lst.count(3) == 2:
+			return 25
+		elif lst.count(4) == 2:
+			return 25
+		elif lst.count(5) == 2:
+			return 25
+		elif lst.count(6) == 2:
+			return 25
+
+	elif lst.count(3) == 3:
+		if lst.count(2) == 2:
+			return 25
+		elif lst.count(1) == 2:
+			return 25
+		elif lst.countt(4) == 2:
+			return 25
+		elif lst.count(5) == 2:
+			return 25
+		elif lst.count(6) == 2:
+			return 25
+
+	elif lst.count(4) == 3:
+		if lst.count(2) == 2:
+			return 25
+		elif lst.count(3) == 2:
+			return 25
+		elif lst.count(1) == 2:
+			return 25
+		elif lst.count(5) == 2:
+			return 25
+		elif lst.count(6) == 2:
+			return 25
+
+	elif lst.count(5) == 3:
+		if lst.count(2) == 2:
+			return 25
+		elif lst.count(3) == 2:
+			return 25
+		elif lst.count(4) == 2:
+			return 25
+		elif lst.count(1) == 2:
+			return 25
+		elif lst.count(6) == 2:
+			return 25
+
+	elif lst.count(6) == 3:
+		if lst.count(2) == 2:
+			return 25
+		elif lst.count(3) == 2:
+			return 25
+		elif lst.count(4) == 2:
+			return 25
+		elif lst.count(5) == 2:
+			return 25
+		elif lst.count(1) == 2:
+			return 25
+	else:
+		return 0
+
+
+def smst(lst):
+	counter1 = 1
+	while counter1 < 4:
+		if (
+			lst.count(counter1) >= 1
+			and lst.count(counter1 + 1) >= 1
+			and lst.count(counter1 + 2) >= 1
+			and lst.count(counter1 + 3) >= 1
+		):
+			return 30
+		else:
+			counter1 += 1
+	return 0
+
+
+def lgst(lst):
+	if (
+		lst.count(1) == 1
+		and lst.count(2) == 1
+		and lst.count(3) == 1
+		and lst.count(4) == 1
+		and lst.count(5) == 1
+	):
+		return 40
+	elif (
+		lst.count(2) == 1
+		and lst.count(3) == 1
+		and lst.count(4) == 1
+		and lst.count(5) == 1
+		and lst.count(6) == 1
+	):
+		return 40
+	else:
+		return 0
+
+
+def chance(lst):
+	return sum(lst)
+
+
+def yahtzee(lst):
+	if lst.count(1) == 5:
+		return 50
+	elif lst.count(2) == 5:
+		return 50
+	elif lst.count(3) == 5:
+		return 50
+	elif lst.count(4) == 5:
+		return 50
+	elif lst.count(5) == 5:
+		return 50
+	elif lst.count(6) == 5:
+		return 50
+	else:
+		return 0
+		
+
+
+def get_category(name_or_func):
+	"""Returns an index of categories referring to the given category name or function.
+	This should be used to easily and efficiently locate categories.
+	It can convert "fourK" to "Four of a Kind" and viseversa, for example"""
+	for i in range(len(categories)):
+		if categories[i][0].lower() == name_or_func:
+			return i
+		if categories[i][1].__name__ == name_or_func:
+			return i
+		if categories[i][1] == name_or_func:
+			return j
+	return
+
+
+#tuple containing mapping of possible score categories to their corresponding functions
+categories = (
+	("One's", ones),
+	("Two's", twos),
+	("Three's", threes),
+	("Four's", fours),
+	("Five's", fives),
+	("Six's", sixs),
+	("Three of a Kind", threeK),
+	("Four of a Kind", fourK),
+	("Full House", fh),
+	("Small Straight", smst),
+	("Large Straight", lgst),
+	("Chance", chance),
+	("Yahtzee!", yahtzee)
+)
 
 
 def go():
@@ -63,7 +477,7 @@ def go():
 					saved_dice.append(current_roll[item])
 			#else statement to program npc
 			else:
-				category = scoring.best_category(current_roll)
+				category = best_category(current_roll)
 				name = category.__name__
 				print(items)
 				if name == "yahtzee":
@@ -112,7 +526,7 @@ def go():
 			current_roll = roll(5-len(saved_dice))
 			num_rolls += 1
 		if not is_npc(current_player):
-			turn_score = scoring.usr_scores(saved_dice)
+			turn_score = usr_scores(saved_dice)
 		scores[current_player] += turn_score
 		print(str(turn_score)+" "+pluralize("point", turn_score!=1, False))
 		#cycle order
