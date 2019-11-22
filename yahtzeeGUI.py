3745c1837e93843fb855e91ff0c26850d71ebc62
Noel Glamann <nglamann@debian08.CIS.COM>
2019-11-22 08:31:14 -0600

    modified:   yahtzeeGUI.py

--- yahtzeeGUI.py
+++ yahtzeeGUI.py
@@ -5,3 +5,11 @@
 '''constructing a GUI for the Yahtzee game'''
 
 import yahtzee
+import pygame as py
+
+s_width = 1000
+s_height = 850
+
+py.display.init()
+py.display.set_mode((s_width, s_height))
+py.display.set_caption('Yahtzee!')
